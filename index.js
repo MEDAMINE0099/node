7943df2f226b8260dbf4734d64e156928402efb3
MEDAMINE0099 <110159634+MEDAMINE0099@users.noreply.github.com>
2023-03-31 01:12:47 +0200

    ss

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 require('dotenv').config()
+var http = require('http');
+
 
 console.log(process.env.mongourl)
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
